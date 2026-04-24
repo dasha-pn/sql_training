45be2bf1a966ee47b4e1909bf0d64d47ae245f53
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-04-24 18:47:04 +0300

    added the 3 task

---
+++ Nth_highest_salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE offset_val INT;
+  SET offset_val = N - 1;
+
+  RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT offset_val, 1
+  );
+END
+
+-- OFFSET починається з 0, а N — з 1
